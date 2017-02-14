f3ac67ee6d82e2597ba881bbe10063c752937970
zhb <zhanghaibin531@163.com>
2017-02-14 16:18:53 +0800

    Initial Commit

---
+++ EditDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EditDemo
+//
+//  Created by EOS on 17/2/14.
+//  Copyright © 2017年 zhanghaibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
